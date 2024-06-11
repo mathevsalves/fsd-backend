d86f4203bac39c9ce2d5d4ad4205c2c4e0728d1f
Matheus Alves de Amorim Santos <matheus.santos@nava.com.br>
2024-06-11 20:02:25 -0300

    Initial commit

---
+++ src/test/java/com/senac/fsdbackend/ClassApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.fsdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsdBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
